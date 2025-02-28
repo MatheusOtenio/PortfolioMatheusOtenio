e7625208bc8842f6cd116c534c6746cdef5f2b4c
Matheus Otenio <matheus.otenio843@gmail.com>
2025-02-28 18:15:00 -0300

    Scroll Automatico para Projetos

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,18 +1,26 @@
 import React from "react";
-
 import styles from "./Projects.module.css";
-
 import projects from "../../data/projects.json";
 import { ProjectCard } from "./ProjectCard";
 
 export const Projects = () => {
+  // Duplica os projetos para criar o efeito infinito
+  const duplicatedProjects = [...projects, ...projects, ...projects];
+
   return (
     <section className={styles.container} id="projects">
       <h2 className={styles.title}>Projects</h2>
-      <div className={styles.projects}>
-        {projects.map((project, id) => {
-          return <ProjectCard key={id} project={project} />;
-        })}
+      <div className={styles.scrollContainer}>
+        <div className={styles.carousel}>
+          {duplicatedProjects.map((project, id) => (
+            <ProjectCard key={id} project={project} />
+          ))}
+        </div>
+        <div className={`${styles.carousel} ${styles.carouselSecondary}`}>
+          {duplicatedProjects.map((project, id) => (
+            <ProjectCard key={`secondary-${id}`} project={project} />
+          ))}
+        </div>
       </div>
     </section>
   );
