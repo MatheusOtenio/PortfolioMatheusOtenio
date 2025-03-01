facc75f5317aa7cdf15f151a3691b9f74dc292c1
Matheus Otenio <matheus.otenio843@gmail.com>
2025-03-01 17:28:59 -0300

    sla

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import styles from "./App.module.css";
 import { About } from "./components/About/About";
+import Assistant from "./components/Assistant/Assistant";
 import { Contact } from "./components/Contact/Contact";
 import { Experience } from "./components/Experience/Experience";
 import { Hero } from "./components/Hero/Hero";
 import { Navbar } from "./components/Navbar/Navbar";
 import { Projects } from "./components/Projects/Projects";
-import { Assistant } from "./components/Assistant/Assistant";
 
 function App() {
   return (
