1e73bfc1892d328f7604964905810c46d8daca84
Matheus Otenio <matheus.otenio843@gmail.com>
2025-03-03 17:02:31 -0300

    Melhorias de UI

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-
 import styles from "./Contact.module.css";
 import { getImageUrl } from "../../utils";
 
@@ -12,29 +11,47 @@ export const Contact = () => {
       </div>
       <ul className={styles.links}>
         <li className={styles.link}>
-          <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
-          <a href="mailto:matheus.otenio843@gmail.com">
-            matheus.otenio843@gmail.com
+          <a
+            href="#"
+            onClick={(e) => {
+              e.preventDefault();
+              navigator.clipboard.writeText("matheus.otenio843@gmail.com");
+              alert("E-mail copiado para a área de transferência!");
+            }}
+          >
+            <img src={getImageUrl("contact/emailIcon.png")} alt="Email" />
+          </a>
+        </li>
+
+        <li className={styles.link}>
+          <a
+            href="https://www.linkedin.com/in/matheus-otenio/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={getImageUrl("contact/linkedinIcon.png")} alt="LinkedIn" />
           </a>
         </li>
         <li className={styles.link}>
-          <img
-            src={getImageUrl("contact/linkedinIcon.png")}
-            alt="LinkedIn icon"
-          />
-          <a href="https://www.linkedin.com/in/matheus-otenio/">
-            linkedin.com/in/matheus-otenio/
+          <a
+            href="https://github.com/MatheusOtenio"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={getImageUrl("contact/githubIcon.png")} alt="GitHub" />
           </a>
         </li>
         <li className={styles.link}>
-          <img src={getImageUrl("contact/githubIcon.png")} alt="Github icon" />
-          <a href="https://github.com/MatheusOtenio">
-            github.com/MatheusOtenio
+          <a
+            href="#"
+            onClick={(e) => {
+              e.preventDefault();
+              navigator.clipboard.writeText("+55 19 98253-5723");
+              alert("Número copiado para a área de transferência!");
+            }}
+          >
+            <img src={getImageUrl("contact/telefone.png")} alt="Telefone" />
           </a>
-        </li>
-        <li className={styles.link}>
-          <img src={getImageUrl("contact/telefone.png")} alt="Phone icon" />
-          <a>(19) 98253-5723</a>
         </li>
       </ul>
     </footer>
