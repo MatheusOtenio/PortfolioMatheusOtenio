77e46d16ec1c1ddff54989e759b1c9fefb0bfd07
Matheus Otenio <matheus.otenio843@gmail.com>
2026-01-14 13:08:34 -0300

    fix(i18n): rename i18n.js to i18n.jsx and ensure React import to fix Vite JSX parsing

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from "./App.jsx";
 import "./index.css";
 import "@fontsource/outfit";
 import "@fontsource/roboto";
-import { I18nProvider } from "./i18n";
+import { I18nProvider } from "./i18n.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
