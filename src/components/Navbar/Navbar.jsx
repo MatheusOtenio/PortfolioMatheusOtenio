b0e170a17480f788d6f6b851736c5ea9d28880b4
Matheus Otenio <matheus.otenio843@gmail.com>
2026-01-14 11:31:04 -0300

    feat(i18n): navbar texts -> t() + language selector

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,11 @@
 import React, { useState } from "react";
-
 import styles from "./Navbar.module.css";
 import { getImageUrl } from "../../utils";
+import { useI18n } from "../../i18n";
 
 export const Navbar = () => {
   const [menuOpen, setMenuOpen] = useState(false);
+  const { language, setLanguage, t } = useI18n();
 
   return (
     <nav className={styles.navbar}>
@@ -16,7 +17,7 @@ export const Navbar = () => {
               ? getImageUrl("nav/closeIcon.png")
               : getImageUrl("nav/menuIcon.png")
           }
-          alt="menu-button"
+          alt={t("navbar.menuButtonAlt")}
           onClick={() => setMenuOpen(!menuOpen)}
         />
         <ul
@@ -24,21 +25,29 @@ export const Navbar = () => {
           onClick={() => setMenuOpen(false)}
         >
           <li>
-            <a href="#">Início</a>
+            <a href="#">{t("navbar.home")}</a>
           </li>
           <li>
-            <a href="#about">Sobre</a>
+            <a href="#about">{t("navbar.about")}</a>
           </li>
           <li>
-            <a href="#experience">Experiência</a>
+            <a href="#experience">{t("navbar.experience")}</a>
           </li>
           <li>
-            <a href="#projects">Projetos</a>
+            <a href="#projects">{t("navbar.projects")}</a>
           </li>
           <li>
-            <a href="#contact">Contato</a>
+            <a href="#contact">{t("navbar.contact")}</a>
           </li>
         </ul>
+        <select
+          value={language}
+          onChange={(event) => setLanguage(event.target.value)}
+        >
+          <option value="pt">PT</option>
+          <option value="en">EN</option>
+          <option value="jp">JP</option>
+        </select>
       </div>
     </nav>
   );
